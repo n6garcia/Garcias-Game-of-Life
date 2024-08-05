233320d7c89f6367b4b3c418d64cd6300edae152
Noel Garcia <n6garcia@ucsd.edu>
2024-08-05 16:36:12 -0700

    new file

---
+++ noels-gol-futr.py
@@ -0,0 +1,60 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+def initialize_grid(size):
+    return np.random.choice([0, 1], size=(size, size))
+
+
+def count_neighbors(grid, x, y):
+    size = len(grid)
+    return sum(
+        [
+            grid[(x + i) % size][(y + j) % size]
+            for i in [-1, 0, 1]
+            for j in [-1, 0, 1]
+            if (i, j) != (0, 0)
+        ]
+    )
+
+
+def update_grid(grid):
+    size = len(grid)
+    new_grid = np.copy(grid)
+
+    for x in range(size):
+        for y in range(size):
+            neighbors = count_neighbors(
+                new_grid, x, y
+            )  # Use new_grid to count neighbors
+            if grid[x][y] == 1:
+                if neighbors < 2 or neighbors > 3:
+                    new_grid[x][y] = 0  # Huilon becomes vacuumous (dies)
+            else:
+                if neighbors == 3:
+                    new_grid[x][y] = 1  # Huilon becomes spacious (birth)
+    return new_grid
+
+
+def animate_grid(size, steps):
+    grid = initialize_grid(size)
+
+    fig, ax = plt.subplots()
+    ax.set_axis_off()
+    img = ax.imshow(grid, cmap="binary")
+
+    def animate(i):
+        nonlocal grid
+        grid = update_grid(grid)
+        img.set_data(grid)
+        return [img]
+
+    ani = animation.FuncAnimation(
+        fig, animate, frames=steps, blit=True, interval=100, repeat=False
+    )
+    plt.show()
+
+
+# Example usage
+animate_grid(size=50, steps=100)
